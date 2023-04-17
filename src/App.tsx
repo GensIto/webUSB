53dae37b04079bada2c693153dc41ea59478df08
GensIto <genta1121@icloud.com>
2023-04-17 14:03:12 +0900

    Dividerを作りみやすく

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,29 @@ import { AccessUSBDevice } from "./components/AccessUSBDevice";
 import { SelectFile } from "./components/SelectFile";
 import { SelectDirectory } from "./components/SelectDirectory";
 import { PushIndexedDB } from "./components/PushIndexedDB";
+import { Divider } from "./components/Divider";
 
 const App: React.FC = () => {
   return (
     <div>
       <RequestUSB />
+      <Divider />
+      {/*  */}
       <GetUSB />
+      <Divider />
+      {/*  */}
       <AccessUSBDevice />
+      <Divider />
+      {/*  */}
       <SelectFile />
+      <Divider />
+      {/*  */}
       <SelectStartDir />
+      <Divider />
+      {/*  */}
       <SelectDirectory />
+      <Divider />
+      {/*  */}
       <PushIndexedDB />
     </div>
   );
